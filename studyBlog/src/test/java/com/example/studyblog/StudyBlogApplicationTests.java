48897abcfc4d97234897fee88a27352e9536c533
neya0 <wltjd1484@gmail.com>
2022-08-13 09:54:39 +0900

    리드미 꺼내기

---
+++ studyBlog/src/test/java/com/example/studyblog/StudyBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.studyblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyBlogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
