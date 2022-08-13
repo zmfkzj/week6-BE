828098c0f0d0884dc9cef54c2a20b6a35e51feaf
neya0 <wltjd1484@gmail.com>
2022-08-13 09:51:43 +0900

    프로젝트 시작

---
+++ studyBlog/src/main/java/com/example/studyblog/StudyBlogApplication.java
@@ -0,0 +1,13 @@
+package com.example.studyblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyBlogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudyBlogApplication.class, args);
+    }
+
+}
