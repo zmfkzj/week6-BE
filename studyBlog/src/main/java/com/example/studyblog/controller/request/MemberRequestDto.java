76e80084d2438d8de251e0e2cc8abc292459e62a
neya0 <wltjd1484@gmail.com>
2022-08-15 20:51:37 +0900

    회원가입, 로그인, 닉네임 중복 확인, 아이디 중복확인, 로그아웃 완료

---
+++ studyBlog/src/main/java/com/example/studyblog/controller/request/MemberRequestDto.java
@@ -0,0 +1,29 @@
+package com.example.studyblog.controller.request;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+
+import com.example.studyblog.domain.Gender;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class MemberRequestDto {
+
+  @NotBlank
+  private String memberId;
+
+  @NotBlank
+  private String nickname;
+
+  @NotBlank
+  @Size(min = 4, max = 32)
+  @Pattern(regexp = "[a-z\\d]*${3,32}")
+  private String password;
+
+  private Gender gender;
+}
---
+++ studyBlog/src/main/java/com/example/studyblog/repository/MemberRepository.java
@@ -0,0 +1,12 @@
+package com.example.studyblog.repository;
+
+import com.example.studyblog.domain.Member;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MemberRepository extends JpaRepository<Member, Long> {
+
+  Optional<Member> findById(Long id);
+  Optional<Member> findByNickname(String nickname);
+  Optional<Member> findByMamberId(String memberId);
+}
